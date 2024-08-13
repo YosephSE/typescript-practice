fcdeac15230953b38881bf0047fe02751804fcec
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-08-13 15:55:05 +0300

    types in functions

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 var userName = "Yoseph";
 var age = 20;
+age = 19;
+var circumferance = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circumferance(4));
--- index.ts
+++ index.ts
@@ -1,2 +1,10 @@
-const userName: string = "Yoseph"
-const age: number  = 20
+let userName: string = "Yoseph"
+let age: number  = 20
+
+age = 19
+
+const circumferance = (diameter: number): number => {
+    return diameter * Math.PI
+}
+
+console.log(circumferance(4))
